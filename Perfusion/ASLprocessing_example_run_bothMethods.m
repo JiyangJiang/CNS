5396eda5ba97b7b2156720592c61b35a3e764674
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-06-28 17:06:06 +1000

    move ASL to BMP

--- Perfusion/ASLprocessing_example_run_bothMethods.m
+++
@@ -1,6 +0,0 @@
-curr_folder = fileparts (mfilename ('fullpath'));
-
-addpath (curr_folder);
-
-% ASLprocessing_FSL_example_run;
-ASLprocessing_ASLtbx_example_run;
--- Perfusion/oxford_asl_R3-9-13/python/asl/__init__.py
+++
@@ -1,2 +0,0 @@
-# pkg-util style namespace package to allow asl package to be split over multiple locations
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
